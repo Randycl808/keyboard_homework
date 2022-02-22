1d71100f9814b1334aa9aa99211ab3558143ca76
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-02-22 14:21:18 -0800

    update

---
+++ src/keyboard.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+import { getAlphabet } from './Buttons'
+const MessageBasic = ()=>{
+ 
+    // useState function, if this confusing 
+    // spend sometime reading up on useState
+    const [message, setMessage] = useState('')
+
+    const handleClicked = (char) => {
+        setMessage(message + char)
+    }
+
+    const renderKeyboard = ()=>{
+        // get ['a','b'..'z'] and store to alp
+        let alp = getAlphabet()
+        
+        // mapping over that alp array
+        // for each thing I am return this
+        //button key={char} onClick={()=> handleClicked(char)}>{char}</button>
+        // char is the current letter 'a' then 'b' then 'c' etc
+        let jsx = alp.map(char=>{
+            return  <button key={char} onClick={()=> handleClicked(char)}>{char}</button>
+        })
+        return jsx
+    }
+
+    return (
+       <div>
+           <p><u>HI, Here's your Keyboard. ENJOY!</u></p>
+           <p>Keyboard:{message}</p>
+           {renderKeyboard()}
+       </div> 
+    )
+}
+
+export default MessageBasic
